9bc76e13f6a158ea981f47a43e9073f7b16d0492
Hok Shun Poon <hok@hokshunpoon.me>
2015-03-02 15:15:32 +0000

    Demonstrate that popover can animate at the same time as navigation controller’s switching of view controllers when the navigation controller is a child view controller.

--- PopoverDemo/MESViewController.m
+++ PopoverDemo/MESViewController.m
@@ -7,31 +7,47 @@
 //
 
 #import "MESViewController.h"
-#import "MESContentViewController.h"
+#import "RBStoryboardLink.h"
 
-@interface MESViewController ()
+@interface MESViewController () <UINavigationControllerDelegate>
+@property(nonatomic, strong) RBStoryboardLink *storyboardLink;
 @end
 
 @implementation MESViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    MESContentViewController *content = [self.storyboard instantiateViewControllerWithIdentifier:@"PopoverContentController"];
-    
-	// Setup the popover for use in the detail view.
-	self.popover = [[UIPopoverController alloc] initWithContentViewController:content];
+
+    // Do any additional setup after loading the view, typically from a nib.
+    self.storyboardLink = [self.storyboard instantiateViewControllerWithIdentifier:@"link"];
+
+    UINavigationController *navigationController = (UINavigationController *) self.storyboardLink.scene;
+    navigationController.delegate = self;
+
+    // Setup the popover for use in the detail view.
+    self.popover = [[UIPopoverController alloc] initWithContentViewController:self.storyboardLink];
 //	self.popover.popoverContentSize = CGSizeMake(320., 320.);
-	self.popover.delegate = self;
+//    self.popover.delegate = self;
 }
 
-- (IBAction)showPopover:(UIButton *)sender {
-    // Set the sender to a UIButton.
-    UIButton *tappedButton = (UIButton *)sender;
+- (IBAction)didTouchUpInsideShowPopoverButton:(UIButton *)sender {
+    UIButton *tappedButton = (UIButton *) sender;
 
     // Present the popover from the button that was tapped in the detail view.
     [self.popover presentPopoverFromRect:tappedButton.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+}
+
+#pragma mark - Nav Del
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    NSAssert(self.storyboardLink, @"Storyboard link should be present.");
+
+    CGSize size = viewController.preferredContentSize;
+
+    NSLog(@"Setting the storyboardLink's preferred content size to %@", NSStringFromCGSize(size));
+
+//    CGFloat navbarHeight = navigationController.navigationBar.frame.size.height;
+    self.storyboardLink.preferredContentSize = size;
 }
 
 @end
